f5e1ac433f3b99a5734aedafeebe3e729e1ecf60
Sword99992 <zeed99992@gmail.com>
2024-12-13 16:23:40 +0700

    Ae001

--- src/main.cpp
+++ src/main.cpp
@@ -1,70 +1,70 @@
 #include <Arduino.h>
-int myFunction(int, int);
-int st1 = 15;
-int ledG1 = 23;
-int ledY1 = 19;
-int ledR1 = 18;
-int ledG2 = 5;
-int ledY2 = 17;
-int ledR2 = 16;
+int led1 = 23;
+int led2 = 19;
+int led3 = 18;
+int led4 = 5;
+int led5 = 17;
+int led6 = 16;
+int led7 = 4;
+int led8 = 8;
+int g1 = 36;
 void setup() {
- pinMode(st1, INPUT);
- pinMode(ledG1, OUTPUT);
- pinMode(ledY1, OUTPUT);
- pinMode(ledR1, OUTPUT);
- pinMode(ledG2, OUTPUT);
- pinMode(ledY2, OUTPUT);
- pinMode(ledR2, OUTPUT);
-}
+    pinMode (led1, OUTPUT);
+    pinMode (led2, OUTPUT);
+    pinMode (led3, OUTPUT);
+    pinMode (led4, OUTPUT);
+    pinMode (led5, OUTPUT);
+    pinMode (led6, OUTPUT);
+    pinMode (led7, OUTPUT);
+    pinMode (led8, OUTPUT);
+ }
 
-void loop() {
-  int a = digitalRead(st1);
-  if (a == HIGH) 
-  {
-    digitalWrite(ledG1, HIGH);
-    digitalWrite(ledY1, HIGH);
-    digitalWrite(ledR1, HIGH);
-    digitalWrite(ledG2, HIGH);
-    digitalWrite(ledY2, HIGH);
-    digitalWrite(ledR2, HIGH);
-    delay(1000);
-    digitalWrite(ledG1, LOW);
-    digitalWrite(ledY1, LOW);
-    digitalWrite(ledR1, LOW);
-    digitalWrite(ledG2, LOW);
-    digitalWrite(ledY2, LOW);
-    digitalWrite(ledR2, LOW);
-    delay(1000);
-  }
-  else
-  {
-    digitalWrite(ledG1, HIGH);
-    digitalWrite(ledY1, LOW);
-    digitalWrite(ledR1, LOW);
-    digitalWrite(ledG2, LOW);
-    digitalWrite(ledY2, LOW);
-    digitalWrite(ledR2, HIGH);
-    delay(1000);
-    digitalWrite(ledG1, LOW);
-    digitalWrite(ledY1, HIGH);
-    digitalWrite(ledR1, LOW);
-    digitalWrite(ledG2, LOW);
-    digitalWrite(ledY2, LOW);
-    digitalWrite(ledR2, HIGH);
-    delay(1000);
-    digitalWrite(ledG1, LOW);
-    digitalWrite(ledY1, LOW);
-    digitalWrite(ledR1, HIGH);
-    digitalWrite(ledG2, HIGH);
-    digitalWrite(ledY2, LOW);
-    digitalWrite(ledR2, LOW);
-    delay(1000);
-    digitalWrite(ledG1, LOW);
-    digitalWrite(ledY1, LOW);
-    digitalWrite(ledR1, HIGH);
-    digitalWrite(ledG2, LOW);
-    digitalWrite(ledY2, HIGH);
-    digitalWrite(ledR2, LOW);
-    delay(1000);
-  }
-}
+ void loop() {
+    int ss1 = analogRead(g1);
+
+    if (ss1 <= 500) 
+    {
+      digitalWrite(led1, HIGH);
+      digitalWrite(led2, LOW);
+      digitalWrite(led3, LOW);
+      digitalWrite(led4, LOW);
+      digitalWrite(led5, LOW);
+      digitalWrite(led6, LOW);
+      digitalWrite(led7, LOW);
+      digitalWrite(led8, HIGH);
+    } else if (ss1 <= 1000)
+    {
+      digitalWrite(led1, HIGH);
+      digitalWrite(led2, HIGH);
+      digitalWrite(led3, HIGH);
+      digitalWrite(led4, LOW);
+      digitalWrite(led5, LOW);
+      digitalWrite(led6, LOW);
+      digitalWrite(led7, LOW);
+      digitalWrite(led8, HIGH);
+    } else if (ss1 <= 2000)
+    {
+      digitalWrite(led1, HIGH);
+      digitalWrite(led2, HIGH);
+      digitalWrite(led3, HIGH);
+      digitalWrite(led4, HIGH);
+      digitalWrite(led5, HIGH);
+      digitalWrite(led6, LOW);
+      digitalWrite(led7, LOW);
+      digitalWrite(led8, HIGH);
+    } else 
+    {
+      digitalWrite(led1, HIGH);
+      digitalWrite(led2, HIGH);
+      digitalWrite(led3, HIGH);
+      digitalWrite(led4, HIGH);
+      digitalWrite(led5, HIGH);
+      digitalWrite(led6, HIGH);
+      digitalWrite(led7, HIGH);
+      digitalWrite(led8, LOW);
+    }
+    
+    
+    
+
+ }
